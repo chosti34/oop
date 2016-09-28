faacba1f3a4099bfc9c47fe78b51f91e5c14c986
Timur <chosti34@gmail.com>
2016-09-29 01:41:52 +0300

    Функция ReplaceString теперь не ищет вхождения подстрок каждую итерацию

--- lab_01/replace/main.cpp
+++ lab_01/replace/main.cpp
@@ -4,65 +4,48 @@
 
 using namespace std;
 
-void OpenFileForReading(ifstream &file, const string &fileName)
+bool OpenFileForReading(ifstream &file, const string &fileName)
 {
     file.open(fileName);
-
-    if (!file.is_open())
-    {
-        cout << "Failed to open " << fileName << " for reading!" << endl;
-        exit(1);
-    }
-
-    if (file.eof())
-    {
-        cout << "File is empty, nothing to read...";
-        exit(1);
-    }
+    return file.is_open();
 }
 
-void OpenFileForWriting(ofstream &file, const string &fileName)
+bool OpenFileForWriting(ofstream &file, const string &fileName)
 {
     file.open(fileName);
-
-    if (!file.is_open())
-    {
-        cout << "Failed to open " << fileName << " for writing!" << endl;
-        exit(1);
-    }
+    return file.is_open();
 }
 
 void ReplaceString(string &str, const string &searchStr, const string &replaceStr)
 {
-    size_t position = 0;
     size_t i = 0;
+    size_t position = str.find(searchStr, i);
 
-    string newStr = "";
+    string newStr;
 
-    while (((position = str.find(searchStr, position)) != string::npos) || (i != str.length()))
+    while ((position != string::npos) || (i != str.length()))
     {
-        if (i == position)
-        {
-            newStr += replaceStr;
-            position += searchStr.length();
-            i += searchStr.length();
-        }
-        else
+        if (i != position)
         {
             newStr += str[i];
             ++i;
         }
+        else
+        {
+            newStr += replaceStr;
+            i += searchStr.length();
+            position = str.find(searchStr, i);
+        }
     }
 
-    str = newStr;
+    str.swap(newStr);
 }
 
-void ReplaceStringsInFile(ifstream &input, ofstream &output, const string &searchStr, const string &replaceStr)
+bool ReplaceStringsInFile(ifstream &input, ofstream &output, const string &searchStr, const string &replaceStr)
 {
     if (searchStr.length() == 0)
     {
-        cout << "String to search cannot be empty" << endl;
-        exit(1);
+        return false;
     }
 
     string currStr;
@@ -70,22 +53,17 @@ void ReplaceStringsInFile(ifstream &input, ofstream &output, const string &searc
     while (getline(input, currStr))
     {
         ReplaceString(currStr, searchStr, replaceStr);
-
         output << currStr << endl;
     }
 
-    if (!output.flush())
-    {
-        cout << "Failed to save data on disk\n";
-        exit(1);
-    }
+    return true;
 }
 
 int main(int argc, char *argv[])
 {
     if (argc != 5)
     {
-        cout << "Invalid arguments count\n"
+        cout << "Invalid amount of arguments!\n"
              << "Usage: replace.exe <input file> <output file> <search string> <replace string>\n";
         return 1;
     }
@@ -93,13 +71,32 @@ int main(int argc, char *argv[])
     ifstream input;
     ofstream output;
 
-    OpenFileForReading(input, argv[1]);
-    OpenFileForWriting(output, argv[2]);
+    if (!OpenFileForReading(input, argv[1]))
+    {
+        cout << "Failed to open " << argv[1] << " for reading!\n";
+        return 1;
+    }
 
-    ReplaceStringsInFile(input, output, argv[3], argv[4]);
+    if (!OpenFileForWriting(output, argv[2]))
+    {
+        cout << "Failed to open " << argv[2] << " for writing!\n";
+        return 1;
+    }
 
-    cout << "All '" << argv[3] << "' was replaced into '"
-         << argv[4] << "' from '" << argv[1] << "' to '" << argv[2] << "'!\n";
+    if (!ReplaceStringsInFile(input, output, argv[3], argv[4]))
+    {
+        cout << "String to search cannot be empty!\n";
+        return 1;
+    }
+
+    if (!output.flush())
+    {
+        cout << "Failed to save data on disk\n";
+        return 1;
+    }
+
+    cout << "All '" << argv[3] << "' was replaced into '" << argv[4] 
+         << "' from '" << argv[1] << "' to '" << argv[2] << "'!\n";
 
     return 0;
 }
