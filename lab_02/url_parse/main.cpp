7e2731c738e4140b91e7b0b1383bb900d4c7ba40
Timur <chosti34@gmail.com>
2016-10-31 14:03:29 +0300

    Добавлен пример работы с регулярными выражениями

--- lab_02/url_parse/main.cpp
+++ lab_02/url_parse/main.cpp
@@ -2,7 +2,29 @@
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    setlocale(LC_ALL, "Russian");
+
+    std::string str;
+    std::regex regularExpression("(\\+|-)?[[:digit:]]+");
+
+    while (true)
+    {
+        std::cout << "Введите число:\n";
+        std::cin >> str;
+
+        if (str == "q")
+        {
+            break;
+        }
+        else if (regex_match(str, regularExpression))
+        {
+            std::cout << "Целое число!\n\n";
+        }
+        else
+        {
+            std::cout << "Не целое число!\n\n";
+        }
+    }
 
     return 0;
 }
