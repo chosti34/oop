cfcbe20d8642ed74a537162b62630568ac73894d
Timur Karimov <chosti34@gmail.com>
2016-11-01 01:07:00 +0300

    Добавлены автотесты к программе url_parse

--- lab_02/url_parse/url_parse_tests/stdafx.h
+++ lab_02/url_parse/url_parse_tests/stdafx.h
@@ -6,6 +6,11 @@
 #include <tchar.h>
 
 #include <iostream>
+#include <string>
+#include <regex>
+
+#include <boost/algorithm/string.hpp>  
+#include <boost/regex.hpp>
 
 #define BOOST_TEST_INCLUDED
 #pragma warning (disable:4702) // To disable 4702 warning in boost
--- lab_02/url_parse/url_parse_tests/tests.cpp
+++ lab_02/url_parse/url_parse_tests/tests.cpp
@@ -1,10 +1,35 @@
 #include "stdafx.h"
+#include "../url_parse.h"
 
-BOOST_AUTO_TEST_SUITE(test_for_tests)
-    BOOST_AUTO_TEST_CASE(compare_two_numbers)
+BOOST_AUTO_TEST_SUITE(ParseURL_function)
+    BOOST_AUTO_TEST_CASE(trying_to_parse_empty_string)
     {
-        int x = 5;
-        int y = 10;
-        BOOST_CHECK(x != y);
+        std::string str;
+        Url url;
+        BOOST_CHECK(!ParseURL(str, url));
+    }
+
+    BOOST_AUTO_TEST_CASE(parsing_url_with_out_of_boundary_values_of_port)
+    {
+        std::string str = "http://www.mysite.com:0/docs/document1.html?page=30&lang=en#title";
+        Url url;
+        BOOST_CHECK(!ParseURL(str, url));
+
+        str = "http://www.mysite.com:65536/docs/document1.html?page=30&lang=en#title";
+        BOOST_CHECK(!ParseURL(str, url));
+    }
+
+    BOOST_AUTO_TEST_CASE(parsing_url_with_invalid_protocol)
+    {
+        std::string str = "xhttp://www.mysite.com:80";
+        Url url;
+        BOOST_CHECK(!ParseURL(str, url));
+    }
+
+    BOOST_AUTO_TEST_CASE(parsing_correct_url_string)
+    {
+        std::string str = "https://ru.wikipedia.org:443/wiki/C%2B%2B";
+        Url url;
+        BOOST_CHECK(ParseURL(str, url));
     }
 BOOST_AUTO_TEST_SUITE_END()
