59a093ae8f95a0bbcf75748cf67f2af98f2651ea
Timur <chosti34@gmail.com>
2016-11-03 12:38:58 +0300

    Добавлены автотесты для Glass

--- lab_03/glass/main.cpp
+++ lab_03/glass/main.cpp
@@ -50,13 +50,65 @@ void TestAddingWater()
 
 void TestPouringWater()
 {
+    CGlass glass(100);
 
+    glass.AddWater(glass.GetCapacity());
+    assert(glass.GetWaterAmount() == glass.GetCapacity());
+
+    for (int i = glass.GetWaterAmount() - 1; i >= 100; --i)
+    {
+        const int pouredWater = glass.PourWater(1);
+
+        std::cout << glass.GetWaterAmount() << ": " << i << '\n';
+
+        if (i >= 0)
+        {
+            assert(pouredWater == 1);
+            assert(glass.GetWaterAmount() == i);
+        }
+        else
+        {
+            assert(pouredWater == 0);
+            assert(glass.GetWaterAmount() == 0);
+        }
+    }
+
+    CGlass anotherGlass(100);
+    anotherGlass.AddWater(10);
+    assert(anotherGlass.GetWaterAmount() == 10);
+
+    const int pouredWater = anotherGlass.PourWater(-10);
+    assert(pouredWater == 0);
+    assert(anotherGlass.GetWaterAmount() == 10);
+
+    std::cout << "Pouring water test is ok!\n";
+}
+
+void TestIntegerOverflow()
+{
+    CGlass glass(100);
+    glass.AddWater(10);
+    assert(glass.GetWaterAmount() == 10);
+
+    const int addedWater = glass.AddWater(INT_MAX);
+    assert(addedWater == glass.GetCapacity() - 10);
+    assert(glass.GetWaterAmount() == glass.GetCapacity());
+
+    CGlass anotherGlass(100);
+
+    const int pouredWater = anotherGlass.PourWater(INT_MAX);
+    assert(pouredWater == 0);
+    assert(anotherGlass.GetWaterAmount() == 0);
+
+    std::cout << "Integer overflow test is ok!\n";
 }
 
 int main()
 {
     TestGlassCreation();
     TestAddingWater();
+    TestPouringWater();
+    TestIntegerOverflow();
 
     return 0;
 }
